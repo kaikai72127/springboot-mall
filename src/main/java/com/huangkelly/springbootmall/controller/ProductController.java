ca40ac293425517a55e28288f4875f8836e23cb5
Kelly Huang <a8522h0721@gmail.com>
2023-01-30 19:20:06 +0800

    商品功能-實作查詢商品列表(分頁)

--- src/main/java/com/huangkelly/springbootmall/controller/ProductController.java
+++ src/main/java/com/huangkelly/springbootmall/controller/ProductController.java
@@ -8,11 +8,16 @@ import com.huangkelly.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import java.util.List;
 
+@Validated //使用Max &Min才有效果
 @RestController
 public class ProductController {
 
@@ -30,13 +35,20 @@ public class ProductController {
     		
     		//排序 Sorting
     		@RequestParam(defaultValue = "created_date") String orderBy,
-    		@RequestParam(defaultValue = "desc") String sort
+    		@RequestParam(defaultValue = "desc") String sort,
+    		
+    		//分頁 Pagination
+    		@RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+    		@RequestParam(defaultValue = "0") @Min(0) Integer offset
     		){
     	ProductQueryParams productQueryParams = new ProductQueryParams();
     	productQueryParams.setCategory(category);
     	productQueryParams.setSearch(search);
     	productQueryParams.setOrderBy(orderBy);
     	productQueryParams.setSort(sort);
+    	productQueryParams.setLimit(limit);
+    	productQueryParams.setOffset(offset);
+    	
     	
         List<Product> productList = productService.getProducts(productQueryParams);
 
